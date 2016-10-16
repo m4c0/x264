68d6266815018e11b223076396c43c358bc1f0a7
Eduardo Costa <ecosta@tpk.com.br>
2016-10-16 22:06:40 +0100

    hax to build with cmake

---
+++ config.h
@@ -0,0 +1,8 @@
+#ifndef X264_CONFIG_H
+#define X264_CONFIG_H
+
+#define HAVE_GPL 1
+#define HAVE_INTERLACED 1
+
+#endif
+
---
+++ x264_config.h
@@ -0,0 +1,10 @@
+#ifndef X264_X264_CONFIG_H
+#define X264_X264_CONFIG_H
+
+#define X264_BIT_DEPTH 8
+#define X264_CHROMA_FORMAT 0
+#define X264_VERSION "vXXX"
+
+#endif
+
+
